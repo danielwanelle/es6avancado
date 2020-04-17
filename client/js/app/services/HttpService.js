131b7b0b0456097557fd369944d583332997e536
Daniel Wanelle <danielwanelle@gmail.com>
2020-04-16 22:45:18 -0300

    Alterado o HttpService.js

--- client/js/app/services/HttpService.js
+++ client/js/app/services/HttpService.js
@@ -1,26 +1,11 @@
 class HttpService {
+    _handleErrors(res){
+        if(!res.ok) throw new Error(res.statusText);
+        return res;
+    }
     get(rota){
-        return new Promise((resolve, reject) =>{
-            let xhr = new XMLHttpRequest();
-            xhr.open('GET', rota);
-            /**
-             * Estados requisisões AJAX;
-             * 0: requisição não iniciada;
-             * 1: conexão estabelecida com o servidor;
-             * 2: requisição recebida;
-             * 3: processando requisição;
-             * 4: requisição concluida e a resposta está pronta.
-             */
-            xhr.onreadystatechange = () => {
-                if(xhr.readyState == 4){
-                    if(xhr.status == 200){
-                        resolve(JSON.parse(xhr.responseText));
-                    }else{
-                        reject(xhr.responseText());
-                    }
-                }
-            };
-            xhr.send();
-        })
+        return fetch(rota)
+            .then(res => this._handleErrors(res))
+            .then(res => res.json());
     }
 }
